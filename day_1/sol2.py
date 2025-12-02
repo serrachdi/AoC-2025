0c17f389c4b10fac1b22857d926db16e0cd4bcb6
serrachdi <sabri@DESKTOP-VRE81OB.localdomain>
2025-12-02 23:12:51 +0100

    remove extra prints

--- day_1/sol2.py
+++ day_1/sol2.py
@@ -5,16 +5,14 @@ dial = 50
 count0 = 0
 for x in f:
     mv = int(x[1:])
+
     if x[0] == 'L':
         mv *= -1
     res = dial + mv
-    print(dial,'  +  ',mv,"  =  ",res)
+
     if res <=0 and dial !=0:
         count0+=1
-        print('pass to 0')
     count0 += abs(res)//100
-    print(abs(res)//100)
 
     dial = res % 100
-    print("######################################################################                          ",count0)
 print(count0)
